0f12bc5083275d86807c19ad77c77fda6f3c0e63
Lucas Rios <lucasriosferreira.564@gmail.com>
2026-01-21 19:36:18 -0300

    feat: implementa listagem e exclusão de alunos via API

--- src/components/Alunos.js
+++ src/components/Alunos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 
 class Alunos extends React.Component{
 
@@ -8,15 +8,38 @@ class Alunos extends React.Component{
 
         this.state = {
             alunos : [
-                {'id':1, 'nome':'Luiz Fernando', 'email':'luiz@gmail.com'},
-                {'id':2, 'nome':'João Felipe', 'email':'joao@gmail.com'}
+                
             ]
         }
     }
 
-    render(){
-        return (
-        <Table striped bordered hover>
+    componentDidMount(){
+        this.buscarAluno();
+    }
+
+    componentWillUnmount(){
+        
+    }
+
+    buscarAluno = () => {
+        fetch("https://6971465278fec16a63007800.mockapi.io/alunos")
+         .then(resposta => resposta.json())
+         .then(dados => {
+            this.setState({alunos : dados})
+         });
+    };
+
+    deletarAluno = (id) => {
+        fetch("https://6971465278fec16a63007800.mockapi.io/alunos/"+id, {method: 'DELETE'})
+        .then(resposta => {
+            if(resposta.ok){
+                this.buscarAluno();
+            }
+        });
+    }
+
+    renderTabela() {
+        return <Table striped bordered hover>
             <thead>
                 <tr>
                     <th>Nome</th>
@@ -25,20 +48,26 @@ class Alunos extends React.Component{
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>{this.state.alunos[0].nome}</td>
-                    <td>Luiz@gmail.com</td>
-                    <td>Atualizar Excluir</td>
-                </tr>
-                <tr>
-                    <td>Joao</td>
-                    <td>Joao@gmail.com</td>
-                    <td>Atualizar Excluir</td>
-                </tr>
+                {
+                    this.state.alunos.map((aluno) => 
+                        <tr>
+                            <td>{aluno.nome}</td>
+                            <td>{aluno.email}</td>
+                            <td>Atualizar <Button variant="danger" onClick={() => this.deletarAluno(aluno.id)}>Excluir</Button></td>
+                        </tr>
+                    )
+                }
             </tbody>
         </Table>
-    )
     }
+
+    render() {
+    return (
+        <div>
+            {this.renderTabela()}
+        </div>
+    );
+ }
 }
 
 export default Alunos;
